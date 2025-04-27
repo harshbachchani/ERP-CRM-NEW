b0bdbd950ce18f97453d91b1aa11219b22362667
harshbachchani <harshbachchani9900@gmail.com>
2025-04-27 16:52:45 +0530

    feat: added the code

---
+++ backend/src/settings/useAppSettings.js
@@ -0,0 +1,8 @@
+const useAppSettings = () => {
+  let settings = {};
+  settings['idurar_app_email'] = 'no-reply@uvigaming.com';
+  settings['idurar_base_url'] = 'https://uvigaming.com';
+  return settings;
+};
+
+module.exports = useAppSettings;
